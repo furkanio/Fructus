301895ffeb783da8c3c30d2f38cb5003984989f0
Furkan Yıldız <furkan@trueyogi.com>
2023-08-03 01:14:08 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Furkan Yildiz on 3.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
